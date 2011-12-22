732925fc8f9efc311dddf2cdd7546f8620793560
Johannes Plunien <plu@pqpq.de>
2011-12-22 05:41:31 +0100

    Add MetaCPAN::Contest::Vote::Model::Entries.

---
+++ lib/MetaCPAN/Contest/Vote/Model/Entries.pm
@@ -0,0 +1,15 @@
+package MetaCPAN::Contest::Vote::Model::Entries;
+
+use strict;
+use warnings;
+use base qw(Catalyst::Model::Adaptor);
+
+__PACKAGE__->config( class => 'MetaCPAN::Contest::Vote::Entries' );
+
+=head1 NAME
+
+MetaCPAN::Contest::Vote::Model::Entries
+
+=cut
+
+1;
