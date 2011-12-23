57368a49273d51309586d718dc8a7d5eaaf18834
Johannes Plunien <plu@pqpq.de>
2011-12-23 05:24:48 +0100

    Let Entries model make a new instance per request.

--- lib/MetaCPAN/Contest/Vote/Model/Entries.pm
+++ lib/MetaCPAN/Contest/Vote/Model/Entries.pm
@@ -2,7 +2,7 @@ package MetaCPAN::Contest::Vote::Model::Entries;
 
 use strict;
 use warnings;
-use base qw(Catalyst::Model::Adaptor);
+use base qw(Catalyst::Model::Factory::PerRequest);
 
 __PACKAGE__->config( class => 'MetaCPAN::Contest::Vote::Entries' );
 
